da319319e34ca11de6fe722e8b54102bba0f32e0
gwenwu <gwnwu@outlook.com>
2025-03-23 21:49:52 +0800

    add ship behavior

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -12,6 +13,8 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # 设置背景色
         self.bg_color = (230,230,230) # (r,g,b)
@@ -26,6 +29,7 @@ class AlienInvasion:
 
             # 每次循环时都重绘屏幕
             self.screen.fill(self.settings.bg_colder)
+            self.ship.blitme()
             # 让最近绘制的屏幕可见
             pygame.display.flip()
             self.clock.tick(60) #帧率60，pygame将尽可能确保这个循环每秒恰好运行60次
